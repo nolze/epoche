542df2e4959981175f2ad9facb58f7ddf1269cc9
nolze <nolze@archlinux.us>
2022-02-22 02:46:43 +0900

    chore(view): fix rollup

--- view/rollup.config.js
+++ view/rollup.config.js
@@ -2,6 +2,7 @@ import svelte from 'rollup-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
@@ -54,6 +55,7 @@ export default {
 		// some cases you'll need additional configuration -
 		// consult the documentation for details:
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		json(),
 		resolve({
 			browser: true,
 			dedupe: ['svelte']
